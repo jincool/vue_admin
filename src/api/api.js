bcbdfd1247918c2c42fd26ccb3ce3bbfbe41dacb
jincool <634596685@qq.com>
2019-06-10 10:22:56 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Qs from 'qs'
 let Axios= axios.create({
-    baseURL:'/',
+    baseURL:'/api/',
     transformRequest: [function (data) { // <--- here 转换数据
         data = Qs.stringify(data); // 通过Qs.stringify转换为表单查询参数
         return data;
@@ -10,14 +10,13 @@ let Axios= axios.create({
         'Content-Type':'application/x-www-form-urlencoded'
     }
 });
-Axios.defaults.baseURL = 'http://localhost/admin/';
-const api = {
-  login (username, password) {
-    let params={
-      'username':username, 'password':password
-    }
-    return Axios.post('?a=menu',params)
-  }
-};
+// const api = {
+//   login (username, password) {
+//     let params={
+//       'username':username, 'password':password
+//     }
+//     return Axios.post('?a=menu',params)
+//   }
+// };
 
-export default api
+export default Axios
